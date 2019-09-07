913d61f4899341043a4468003eb3a3787bf54b8a
Foissac-Ncola <nicola.foissac@gmail.com>
2019-09-07 23:02:26 +0200

    feat: protect map again player walk outside map

--- controller/controller.cpp
+++ controller/controller.cpp
@@ -9,30 +9,46 @@ Controller::Controller(Model *model) : m_model(model)
 
 void Controller::manageInput(char input)
 {
+
     int newCoordX = m_model->m_player->getX();
     int newCoordY = m_model->m_player->getY();
+    
+    // this need to check if player was in limit with next map.
+    // then if next tile was walkable.
 
-    switch(input)
+    bool modelShouldUpdate = false;
+
+    if(
+          newCoordX <= player_const::MOVE_LIMIT_MIN ||
+          newCoordX >= player_const::MOVE_LIMIT_MAX ||
+          newCoordY >= player_const::MOVE_LIMIT_MAX ||
+          newCoordY <= player_const::MOVE_LIMIT_MIN   )
     {
-    case 'z':
-        newCoordY -= Player::MOVE_STEP;
-        break;
+       std::cerr << "should update" << std::endl;
+    }
+    else {
+       switch(input)
+       {
+          case 'z':
+             newCoordY -= Player::MOVE_STEP;
+             break;
 
-    case 's':
-        newCoordY += Player::MOVE_STEP;
-        break;
+          case 's':
+             newCoordY += Player::MOVE_STEP;
+             break;
 
-    case 'q':
-        newCoordX -= Player::MOVE_STEP;
-        break;
+          case 'q':
+             newCoordX -= Player::MOVE_STEP;
+             break;
 
-    case 'd':
-        newCoordX += Player::MOVE_STEP;
-        break;
+          case 'd':
+             newCoordX += Player::MOVE_STEP;
+             break;
 
-    default:
-        std::cout << "Input not recognized" << std::endl;
-        break;
+          default:
+             std::cout << "Input not recognized" << std::endl;
+             break;
+       }
     }
 
     if (m_model->m_tileMap->checkCollisionWithGraphicObject(GraphicObject(Coordinates(newCoordX, newCoordY), m_model->m_player->getSize())))
--- model/player.h
+++ model/player.h
@@ -2,12 +2,16 @@
 #define PLAYER_H
 
 #include "character.h"
-
+namespace player_const {
+   static const unsigned int MOVE_LIMIT_MIN = 16;
+   static const unsigned int MOVE_LIMIT_MAX = 656;
+}
 class Player : public Character
 {
 public:
     const static int PLAYER_WIDTH = 32;
     const static int MOVE_STEP = 16;
+
 
     Player();
 
